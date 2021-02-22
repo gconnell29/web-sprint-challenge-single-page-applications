eaad2afe7378dd264cb764c8b7320e3d0b9fb10d
gconnell29 <gconnelljr29@gmail.com>
2021-02-21 22:10:34 -0500

    created basic header with nav

--- src/App.js
+++ src/App.js
@@ -2,10 +2,15 @@ import React from "react";
 
 const App = () => {
   return (
-    <>
-      <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
-    </>
+    <div className="app">
+      <header>
+        <h1>Lambda Eats</h1>
+        <nav>
+          <button>Home</button>
+          <button>Help</button>
+        </nav>
+      </header>
+    </div>
   );
 };
 export default App;
